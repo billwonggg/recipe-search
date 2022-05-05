2bc3496f5e41353ff2af5b98f67831fb69a280d9
Bill <billwong7788@gmail.com>
2022-05-05 20:50:20 +1000

    changed props for recipe

---
+++ src/components/Modal.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import { Backdrop, Box, Modal, Typography, Fade } from "@material-ui/core";
+
+const Modal = () => {
+  return <Modal></Modal>;
+};
+
+export default Modal;
