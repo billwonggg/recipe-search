ab7f66f5603405a2a4c48c22d89117f9f8273e57
Bill <billwong7788@gmail.com>
2022-05-06 10:13:32 +1000

    improved modal styles

--- src/components/InfoModal.js
+++ src/components/InfoModal.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Backdrop, Box, Modal, Typography, Fade } from "@material-ui/core";
+import React from "react";
+import { Backdrop, Box, Modal, Fade } from "@material-ui/core";
 import { Table, TableBody, TableRow, TableCell } from "@material-ui/core";
 import { TableContainer, TableHead, Paper } from "@material-ui/core";
 
@@ -12,7 +12,7 @@ const InfoModal = ({ modalRecipe, setModalRecipe }) => {
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    width: "400px",
+    width: "500px",
     maxWidth: "90%",
     bgcolor: "background.paper",
     border: "5px solid #000",
@@ -25,19 +25,17 @@ const InfoModal = ({ modalRecipe, setModalRecipe }) => {
     setModalRecipe(null);
   };
 
-  const calories = modalRecipe.calories;
   const scale = modalRecipe.totalWeight / 100;
   const elements = [
     "ENERC_KCAL",
     "PROCNT",
     "FAT",
-    "CHOLE",
     "CHOCDF",
-    "FIBTG",
     "SUGAR",
+    "CHOLE",
+    "FIBTG",
     "NA",
     "CA",
-    "FE",
     "K",
   ];
   const nutrition = elements.map((e) => {
@@ -58,44 +56,46 @@ const InfoModal = ({ modalRecipe, setModalRecipe }) => {
     >
       <Fade in={true}>
         <Box sx={style}>
-          <Typography id="transition-modal-title" variant="h6" component="h2">
-            More Information
-          </Typography>
-          <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-            Calories: {Math.ceil(calories)}
-          </Typography>
-          <Typography id="transition-modal-description" sx={{ mt: 4 }}>
+          <Box sx={{ fontSize: 35 }}>More Information</Box>
+          <Box sx={{ fontSize: 18, mt: 2 }}>
+            Dish Type: {modalRecipe.dishType[0] ?? "Normal Dish"}
+          </Box>
+
+          <Box sx={{ fontSize: 18, mt: 2 }}>
             Top 5 Health Labels:
-            {modalRecipe.healthLabels.slice(0, 5).map((label, i) => (
-              <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-                {i + 1}. {label}
-              </Typography>
-            ))}
-          </Typography>
-          <TableContainer component={Paper}>
-            <Table
-              sx={{ minWidth: 500 }}
-              size="small"
-              aria-label="a dense table"
-            >
-              <TableHead>
-                <TableRow>
-                  <TableCell></TableCell>
-                  <TableCell align="right">Avg. Quantity per 100g</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {nutrition.map((n) => (
-                  <TableRow key={n.label} align="right">
-                    <TableCell>{n.label}</TableCell>
-                    <TableCell align="right">
-                      {(n.quantity / scale).toFixed(2)} {n.unit}
-                    </TableCell>
+            <ol style={{ fontSize: 14 }}>
+              {modalRecipe.healthLabels.slice(0, 5).map((label) => (
+                <li>{label}</li>
+              ))}
+            </ol>
+          </Box>
+          <Box sx={{ fontSize: 23, mt: 3 }}>
+            Nuitrition Information
+            <TableContainer component={Paper}>
+              <Table
+                sx={{ minWidth: 500 }}
+                size="small"
+                aria-label="a dense table"
+              >
+                <TableHead>
+                  <TableRow>
+                    <TableCell></TableCell>
+                    <TableCell align="right">Avg. Quantity per 100g</TableCell>
                   </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+                </TableHead>
+                <TableBody>
+                  {nutrition.map((n) => (
+                    <TableRow key={n.label} align="right">
+                      <TableCell>{n.label}</TableCell>
+                      <TableCell align="right">
+                        {(n.quantity / scale).toFixed(1)} {n.unit}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Box>
         </Box>
       </Fade>
     </Modal>
