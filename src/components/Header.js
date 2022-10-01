403aa5a49508d777fbb387f2a9dcb82dd52699d1
Bill Wong <billwong7788@gmail.com>
2022-10-01 23:23:10 +1000

    use new search bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,23 @@
 import React from "react";
-import style from "./header.module.css";
 import PulseLoader from "react-spinners/PulseLoader";
-import SearchBar from "material-ui-search-bar";
+import style from "./header.module.css";
+import SearchBar from "./SearchBar";
 
-const Header = ({ search, getSearch, setSearch }) => {
+const Header = ({ search, submit, setSearch }) => {
   return (
     <div className={style.header}>
       <h1>Recipe Search</h1>
       <p>
-        This website's data is sourced from the Edamam Recipe Search API, which
-        has over 2.3 million recipes across the world. <br />
+        This website's data is sourced from the Edamam Recipe Search API, which has over 2.3 million
+        recipes across the world. <br />
         <br />
-        Search up ANY kind of food (e.g. miso ramen, croissant, brownie) to see
-        it in action. <br />
+        Search up ANY kind of food (e.g. miso ramen, croissant, brownie) to see it in action. <br />
         <br />
         Click on a recipe to learn more. &nbsp;
-        <PulseLoader
-          color={"#00539c"}
-          loading={true}
-          size={8}
-          speedMultiplier={0.7}
-        />
+        <PulseLoader color={"#00539c"} loading={true} size={8} speedMultiplier={0.7} />
       </p>
-      <SearchBar
+      <SearchBar search={search} submit={submit} setSearch={setSearch} />
+      {/* <SearchBar
         value={search}
         onChange={(e) => setSearch(e)}
         placeholder="Enter your favourite dish here"
@@ -35,7 +30,7 @@ const Header = ({ search, getSearch, setSearch }) => {
           borderRadius: "15px",
           padding: "3px",
         }}
-      />
+      /> */}
     </div>
   );
 };
