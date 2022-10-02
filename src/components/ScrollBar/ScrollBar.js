2811eafbbf9dd9e3c5b0b9c91b9f45eb2f4957c4
Bill Wong <billwong7788@gmail.com>
2022-10-03 10:51:05 +1100

    move file to new folder

---
+++ src/components/ScrollBar/ScrollBar.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import "./scrollbar.css";
+
+const ScrollBar = () => {
+  const [scroll, setScroll] = useState(0);
+
+  const handleScroll = () => {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+    setScroll(scrolled);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
+
+  return (
+    <div className="scrollbar">
+      <div className="progress-container">
+        <div className="progress-bar" id="myBar" style={{ width: `${scroll}%` }}></div>
+      </div>
+    </div>
+  );
+};
+
+export default ScrollBar;
