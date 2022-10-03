8f091d4019a388fb9e13fb7ec6ea7c9f0eedbe78
Bill Wong <billwong7788@gmail.com>
2022-10-03 22:29:41 +1100

    increase margin of skeleton

--- src/components/RecipeSkeleton.js
+++ src/components/RecipeSkeleton.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const RecipeSkeleton = () => {
   return Array.from({ length: 4 }, (_, i) => (
-    <Stack spacing={1} key={i} m={2}>
+    <Stack spacing={1} key={i} marginLeft="5%" marginRight="5%">
       <Skeleton variant="text" sx={{ height: "8vh" }} />
       <Skeleton variant="rounded" sx={{ maxWidth: "700px", width: "90vw", height: "30vh" }} />
     </Stack>
