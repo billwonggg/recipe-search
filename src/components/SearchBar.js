34da9387d9c0cd73ef87f9bc69dac4e8c1fd8ad2
Bill Wong <billwong7788@gmail.com>
2022-10-03 11:21:36 +1100

    make own searchbar instead of module

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,11 +2,22 @@ import SearchIcon from "@mui/icons-material/Search";
 import { IconButton, InputBase, Paper } from "@mui/material";
 
 const SearchBar = ({ search, submit, setSearch }) => (
-  <Paper component="form" sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 400 }}>
+  <Paper
+    component="form"
+    sx={{
+      m: "10px",
+      p: "3px 7px",
+      display: "flex",
+      alignItems: "center",
+      width: "90%",
+      maxWidth: "600px",
+      borderRadius: "15px",
+    }}
+  >
     <InputBase
       sx={{ ml: 1, flex: 1 }}
-      placeholder="Search Google Maps"
-      inputProps={{ "aria-label": "search google maps" }}
+      placeholder="Search your favourite recipe"
+      inputProps={{ "aria-label": "Search your favourite recipe" }}
       value={search}
       onChange={(e) => setSearch(e.target.value)}
     />
