523ddb4a052e0986334fbb1439894cf48ef9ebd6
Bill Wong <billwong7788@gmail.com>
2022-10-03 17:56:25 +1100

    add handler for enter key

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,35 +1,39 @@
 import SearchIcon from "@mui/icons-material/Search";
 import { IconButton, InputBase, Paper } from "@mui/material";
 
-const SearchBar = ({ search, submit, setSearch }) => (
-  <Paper
-    component="form"
-    sx={{
-      m: "10px",
-      p: "3px 7px",
-      display: "flex",
-      alignItems: "center",
-      width: "90%",
-      maxWidth: "600px",
-      borderRadius: "15px",
-    }}
-  >
-    <InputBase
-      sx={{ ml: 1, flex: 1 }}
-      placeholder="Search your favourite recipe"
-      inputProps={{ "aria-label": "Search your favourite recipe" }}
-      value={search}
-      onChange={(e) => setSearch(e.target.value)}
-    />
-    <IconButton
-      type="button"
-      sx={{ p: "10px" }}
-      aria-label="search"
-      onClick={(e) => submit(e.target.value)}
+const SearchBar = ({ search, submit, setSearch }) => {
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      submit(e);
+    }
+  };
+
+  return (
+    <Paper
+      component="form"
+      sx={{
+        m: "10px",
+        p: "3px 7px",
+        display: "flex",
+        alignItems: "center",
+        width: "90%",
+        maxWidth: "600px",
+        borderRadius: "15px",
+      }}
     >
-      <SearchIcon />
-    </IconButton>
-  </Paper>
-);
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="Search your favourite recipe"
+        inputProps={{ "aria-label": "Search your favourite recipe" }}
+        value={search}
+        onKeyDown={handleKeyDown}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <IconButton type="button" sx={{ p: "10px" }} aria-label="search" onClick={submit}>
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
+};
 
 export default SearchBar;
